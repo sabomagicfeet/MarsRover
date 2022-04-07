44826916cf7c0f7a22fe9db8ae744eaace89e8c3
sabo <sabo@jihyes-mbp.home>
2022-04-07 16:11:58 +0100

    add Test(red) changeDirectionTest() . havn't written changeDirection in Rover class yet

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -11,9 +11,27 @@ public class RoverTest {
 
         Rover rover = new Rover();
 
-        String expected = "A Rover is dropped at 0, 0, Ndfs";
+        String expected = "A Rover is dropped at 0, 0, N";
         String actual = rover.dropDownRover(x, y, direction);
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void changeDirectionTest() {
+        int x = 0;
+        int y = 0;
+        char direction = 'N';
+
+        Rover rover = new Rover();
+
+        String expected = "A Rover is at 0, 0 and facing East";
+        rover.dropDownRover(x, y, direction);
+        rover.changeDirection('R');
+
+        String actual = rover.status();
+
+        Assertions.assertEquals(expected, actual);
+
     }
 }
